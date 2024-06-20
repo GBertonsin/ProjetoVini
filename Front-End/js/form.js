723a8f102b52ef200bec2e8af3f94f706fd95362
Gbertonsin <gabriel_bertonsin@hotmail.com>
2024-06-20 13:59:03 -0300

    feat: :sparkles: Inserir novos campos formulário

--- Front-End/js/form.js
+++ Front-End/js/form.js
@@ -1,21 +1,30 @@
 function getData() {
-  const nome = document.getElementById('nome').value
-  const idade = document.getElementById('idade').value
-  const peso = document.getElementById('peso').value
-  const altura = document.getElementById('altura').value
+  const nome = document.getElementById("nome").value;
+  const idade = document.getElementById("idade").value;
+  const peso = document.getElementById("peso").value;
+  const altura = document.getElementById("altura").value;
 
-  return { nome, idade, peso, altura }
+  return { nome, idade, peso, altura };
 }
 
-function submitData(){
-  const data = getData()
+function submitData() {
+  const data = getData();
 
-  fetch('http://localhost:3000/documento', {
-    method: 'POST',
+  fetch("http://localhost:3000/documento", {
+    method: "POST",
     body: JSON.stringify(data),
     headers: {
-      "Content-Type": 'application/json'
-    }
-  }).then(response=>response.json()).then(data=> console.log(data)).catch(error=>console.log(error.message))
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error.message));
+}
 
+function selecionarComentario() {
+  var select = document.getElementById("comentario");
+  var input = document.getElementById("comentarioSelecionado");
+  var selectedOption = select.options[select.selectedIndex].value;
+  input.value = selectedOption;
 }
